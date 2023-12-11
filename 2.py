c7b0a6d4627f4e615772f5f9505f626b785f4b1e
Osmian101 <r093r25@gmail.com>
2023-12-11 12:49:06 -0500

    part 1 done

--- 2.py
+++ 2.py
@@ -1,25 +1,41 @@
 def main():
-
-    max = {"red":12, "green":13, "blue":14}
+    MAX_RED = 12
+    MAX_GREEN = 13
+    MAX_BLUE = 14
 
     file = open("2.in")
-    total = 0
+    total = 5050 
     for game in file:
-        print(game)
         split_game = game.split(" ")
         game_id = int(split_game[1][:-1])
         print("GAME ID: ", game_id)
         pairs = game.split(" ")[2:]
-        amt_clr = pairs[0], pairs[1].strip(',')
-        print(amt_clr)
-        match amt_clr[1]:
-            case "red":
-                total += game_id if int(amt_clr[0]) < max["red"] else 0
-            case "green":
-                total += game_id if int(amt_clr[0]) < max["green"] else 0
-            case "blue":
-                total += game_id if int(amt_clr[0]) < max["blue"] else 0
-    print(total)
+        print(pairs)
+        i = 0
+        length = len(pairs)//2
+        # print("TOTAL: ", total)
+        while (i < length): 
+            amt_clr = pairs[0], pairs[1].strip(',;\n')
+            print(amt_clr)
+            match amt_clr[1]:
+                case "red":
+                    if (int(amt_clr[0]) > MAX_RED):
+                        i += 1
+                        total -= game_id
+                        break
+                case "green":
+                    if (int(amt_clr[0]) > MAX_GREEN):
+                        i += 1
+                        total -= game_id
+                        break
+                case "blue":
+                    if (int(amt_clr[0]) > MAX_BLUE):
+                        i += 1
+                        total -= game_id
+                        break
+            pairs = pairs[2:]
+            i += 1
 
+    print(total)
 
 main()
