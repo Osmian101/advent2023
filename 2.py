ca65e86fa7c26fe774a22ab9095274ede1b7c15c
Osmian101 <r093r25@gmail.com>
2023-12-11 11:23:17 -0500

    max checking works

--- 2.py
+++ 2.py
@@ -3,11 +3,23 @@ def main():
     max = {"red":12, "green":13, "blue":14}
 
     file = open("2.in")
+    total = 0
     for game in file:
         print(game)
         split_game = game.split(" ")
         game_id = split_game[1][:-1]
-        print(game_id)
-        for eme in game.split(" ")[2:]:
-            print(eme)
+        print("GAME ID: " + game_id)
+        pairs = game.split(" ")[2:]
+        amt_clr = pairs[0], pairs[1].strip(',')
+        print(amt_clr)
+        match amt_clr[1]:
+            case "red":
+                total += int(amt_clr[0]) if int(amt_clr[0]) < max["red"] else 0
+            case "green":
+                total += int(amt_clr[0]) if int(amt_clr[0]) < max["green"] else 0
+            case "blue":
+                total += int(amt_clr[0]) if int(amt_clr[0]) < max["blue"] else 0
+    print(total)
+
+
 main()
