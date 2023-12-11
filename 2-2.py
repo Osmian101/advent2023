14f4e9a438c70c09ceb95c5d02f9718a8da7173b
Osmian101 <r093r25@gmail.com>
2023-12-11 13:50:55 -0500

    2-2 done

--- 2-2.py
+++ 2-2.py
@@ -1,46 +1,41 @@
 def main():
-    MAX_RED = 12
-    MAX_GREEN = 13
-    MAX_BLUE = 14
-
     file = open("2.in")
-    total = 5050 
+    powers = 0
     for game in file:
         print(game)
         split_game = game.split(":")
-        print(split_game)
         game_id = int(split_game[0][5:])
         print("GAME ID: ", game_id)
         rounds = split_game[1].split(';')
         print("ROUNDS: ", rounds)
+        highest_red = 0
+        highest_blue = 0
+        highest_green = 0
         for round in rounds:
             pairs = round.strip(" ").split(" ")
-            print("PAIRS: ", pairs)
-            if (pairs[0] == 'Game'):
-                print("SKIP")
-                continue 
             i = 0
             length = len(pairs)//2
             while (i < length): 
                 amt_clr = int(pairs[0]), pairs[1].strip(' ,;\n')
                 match amt_clr[1]:
                     case "red":
-                        if (amt_clr[0] > MAX_RED):
-                            i += 1
-                            total -= game_id
-                            break
+                        if (amt_clr[0] > highest_red):
+                            highest_red = amt_clr[0]
+                        i += 1
                     case "green":
-                        if (amt_clr[0] > MAX_GREEN):
-                            i += 1
-                            total -= game_id
-                            break
+                        if (amt_clr[0] > highest_green):
+                            highest_green = amt_clr[0]
+                        i += 1
                     case "blue":
-                        if (amt_clr[0] > MAX_BLUE):
-                            i += 1
-                            total -= game_id
-                            break
+                        if (amt_clr[0] > highest_blue):
+                            highest_blue = amt_clr[0]
+                        i += 1
                 pairs = pairs[2:]
-                i += 1
-    print(total)
+        print("HIGHEST RED: ", highest_red)
+        print("HIHGEST GREEN: ", highest_green)
+        print("HIGHEST BLUE: ", highest_blue)
+        power = highest_red * highest_blue * highest_green
+        powers += power
 
+    print(powers)
 main()
