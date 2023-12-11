0f5ac695285b5f0ae97ef3aa59581062e325c900
Osmian101 <r093r25@gmail.com>
2023-12-11 11:39:53 -0500

    adds game id if game was possible

--- 2.py
+++ 2.py
@@ -7,18 +7,18 @@ def main():
     for game in file:
         print(game)
         split_game = game.split(" ")
-        game_id = split_game[1][:-1]
-        print("GAME ID: " + game_id)
+        game_id = int(split_game[1][:-1])
+        print("GAME ID: ", game_id)
         pairs = game.split(" ")[2:]
         amt_clr = pairs[0], pairs[1].strip(',')
         print(amt_clr)
         match amt_clr[1]:
             case "red":
-                total += int(amt_clr[0]) if int(amt_clr[0]) < max["red"] else 0
+                total += game_id if int(amt_clr[0]) < max["red"] else 0
             case "green":
-                total += int(amt_clr[0]) if int(amt_clr[0]) < max["green"] else 0
+                total += game_id if int(amt_clr[0]) < max["green"] else 0
             case "blue":
-                total += int(amt_clr[0]) if int(amt_clr[0]) < max["blue"] else 0
+                total += game_id if int(amt_clr[0]) < max["blue"] else 0
     print(total)
 
 
