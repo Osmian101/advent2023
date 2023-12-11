4390ced9b482540d9669cb9620a4556b5d3f07a6
Osmian101 <r093r25@gmail.com>
2023-12-11 12:50:08 -0500

    cleaned up

--- 2.py
+++ 2.py
@@ -8,28 +8,24 @@ def main():
     for game in file:
         split_game = game.split(" ")
         game_id = int(split_game[1][:-1])
-        print("GAME ID: ", game_id)
         pairs = game.split(" ")[2:]
-        print(pairs)
         i = 0
         length = len(pairs)//2
-        # print("TOTAL: ", total)
         while (i < length): 
-            amt_clr = pairs[0], pairs[1].strip(',;\n')
-            print(amt_clr)
+            amt_clr = int(pairs[0]), pairs[1].strip(',;\n')
             match amt_clr[1]:
                 case "red":
-                    if (int(amt_clr[0]) > MAX_RED):
+                    if (amt_clr[0] > MAX_RED):
                         i += 1
                         total -= game_id
                         break
                 case "green":
-                    if (int(amt_clr[0]) > MAX_GREEN):
+                    if (amt_clr[0] > MAX_GREEN):
                         i += 1
                         total -= game_id
                         break
                 case "blue":
-                    if (int(amt_clr[0]) > MAX_BLUE):
+                    if (amt_clr[0] > MAX_BLUE):
                         i += 1
                         total -= game_id
                         break
