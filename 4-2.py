e4e7bdad01509c13addfcef4496e723aee568a36
Osmian <r093r25@gmail.com>
2023-12-22 18:11:55 -0500

    got a visual representation working

--- 4-2.py
+++ 4-2.py
@@ -1,6 +1,7 @@
 # traverse backward through the won scratch cards, 
 
 def main():
+    ans = 0
     totals = {}
     queue = []
     file = open("4.in")
@@ -15,15 +16,23 @@ def main():
             if (number in played_numbers):
                 i += 1
                 continue
-        totals.update({game_id: i})
+        totals.update({game_id: [i]})
     
+    print(totals)
     card_id = 1
     while (totals):
         bonus_cards = totals.pop("Card{:>4}".format(str(card_id)))
         print(bonus_cards)
+        add_cards_to = range(1, bonus_cards[0] + 1)
+        for x in add_cards_to:
+            totals["Card{:>4}".format(str(card_id + x))].append(bonus_cards[0])
+        while (bonus_cards):
+            #print(bonus_cards.pop(0))
+            ans += bonus_cards.pop(0)
+            #print(ans)
         card_id += 1
     # go through each card one at a time, 
     # new card -> add each bonus card to the queue, i + 1
-
+    print(ans)
 
 main()
